54d0b1c39570d1f5cd23e196528214310b88e862
Alli Nilles <anilles137@gmail.com>
2020-05-04 11:37:15 -0500

    add more environment generation tools

--- generate_random_polygon.py
+++ generate_random_polygon.py
@@ -5,20 +5,18 @@
 
 # `pip install cyaron` should work 
 
-# In[1]:
-
-
 from cyaron import Polygon, Vector
 import shapely
+from shapely.ops import cascaded_union
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.linalg as la
+import math
+from math import atan2
+import random
+import networkx as nx
 
-
-# ### Visualization
-
-# In[2]:
-
+### Visualization
 
 def draw_polygon(poly_point_list):
     jet = plt.cm.jet
@@ -40,28 +38,26 @@ def draw_polygon(poly_point_list):
 
     plt.show()
 
-
-# ## Method 1: Use the Cyaron library
+### Method 1: Use the Cyaron library
+### --------------------------------
 
 # Generate a random set of points. Find a random poly line connecting the points by divide and conquer. The documentation for the library is not very good and only in Chinese, but the source [here](https://github.com/luogu-dev/cyaron/blob/master/cyaron/polygon.py) is not so hard to read.
 
-# Parameters in `Vector.random`: number of points, position range for each dimension, mode (0=unique 1=repeatable 2=float).
+# Parameters in `Vector.random`:
+    # number of points
+    # position range for each dimension
+    # mode (0=unique 1=repeatable 2=float).
 
-# In[3]:
+#poly = Polygon.simple_polygon(Vector.random(50, [800, 800], 2)).points
+#draw_polygon(poly)
 
 
-poly = Polygon.simple_polygon(Vector.random(50, [800, 800], 2)).points
-draw_polygon(poly)
+### Method 2: the Stackoverflow answer with spikyness control.
+### ----------------------------------------------------------
 
-
-# ## Method 2: the Stackoverflow answer with spikyness control.
 
 # This method can only generate star shaped polygons. The code is taken from [here](https://stackoverflow.com/questions/8997099/algorithm-to-generate-random-2d-polygon).
 
-# In[4]:
-
-
-import math, random
 
 def generatePolygon( ctrX, ctrY, aveRadius, irregularity, spikeyness, numVerts ) :
     '''Start with the centre of the polygon at ctrX, ctrY, 
@@ -116,28 +112,34 @@ def clip(x, min, max) :
     elif( x > max ) :  return max
     else :             return x
 
+# use NxN grid in (irregularity, spikyness) parameter space
+# to sample from space # of "pseudorandom" polygons
+# (as defined by generatePolygon function)
+def generatePolygonsGrid(N, irr_min, irr_max, spike_min, spike_max):
+    irr_vals = np.linspace(irr_min, irr_max, N)
+    spike_vals = np.linspace(spike_min, spike_max, N)
+    polygon_list = []
+    for i in irr_vals:
+        for s in spike_vals:
+            verts = generatePolygon(ctrX=250, ctrY=250, aveRadius=100,
+                                    irregularity=i, spikeyness=s, numVerts=20)
+            polygon_list.append(verts)
+            draw_polygon(verts)
+    return polygon_list
 
-# In[5]:
+#verts = generatePolygon( ctrX=250, ctrY=250, aveRadius=100, irregularity=0.35, spikeyness=0.3, numVerts=20 )
+#draw_polygon(verts)
 
 
-verts = generatePolygon( ctrX=250, ctrY=250, aveRadius=100, irregularity=0.35, spikeyness=0.3, numVerts=20 )
-draw_polygon(verts)
+### Method 3: RGP - Generation of Random Polygonal Objects
+### ------------------------------------------------------
 
-
-# ## Method 3: RGP - Generation of Random Polygonal Objects
 
 # The paper is [here]( http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.56.5609&rep=rep1&type=pdf). For some reason the webpage for this project is not accessible anymore, so here is the Internet Wayback [link](https://web.archive.org/web/20190918003901/http://www.cosy.sbg.ac.at/~held/projects/rpg/rpg.html). I haven't read the paper yet but from the webpage their results is much more systematic than the above too. So it might worth to ask them for the code. 
 
 # ## Method 4: Generate Convex Rooms Connected by Corridors
 
 # Convex polygon generation solution from [here](https://stackoverflow.com/questions/6758083/how-to-generate-a-random-convex-polygon). I modified the code so that the polygon is centered at the origin.
-
-# In[6]:
-
-
-import random
-from math import atan2
-
 
 def to_convex_contour(vertices_count,
                       x_generator=random.random,
@@ -203,43 +205,75 @@ def _to_random_boolean():
     return random.getrandbits(1)
 
 
-# In[7]:
 
 
-num_rooms = 15
-corridors_map = np.array(Polygon.simple_polygon(Vector.random(num_rooms, [800, 800], 2)).points)
-draw_polygon(corridors_map)
+# I use a randomly generated polygon as the room connection map for now.
+# We can also triangulate the polygon to get more corridors or manually specify this.
+#corridors_map = np.array(Polygon.simple_polygon(Vector.random(num_rooms, [800, 800], 2)).points)
+#draw_polygon(corridors_map)
+
+# using a polygon as the map means we need obstacle code to work
+# which it doesn't
+# so we will use a tree
+
+from scipy.spatial import distance_matrix
+
+def random_tree(N):
+    rooms = 200.*np.random.rand(N,2)
+    nodes = {i:rooms[i] for i in range(N)}
+    G = nx.Graph()
+    for i in range(N):
+        for j in range(N):
+            if i != j:
+                G.add_edge(i, j, weight=la.norm(rooms[i]-rooms[j]))
+    T = nx.minimum_spanning_tree(G)
+    corridors = T.edges
+    return corridors, rooms
 
 
-# I use a randomly generated polygon as the room connection map for now. We can also triangulate the polygon to get more corridors or manually specify this. 
 
-# In[8]:
+def genTreeEnv(num_rooms, rooms, corridors):
+
+    polygon_list = []
+    min_room_size = 800
+
+    # Generate convex "room" at each point
+    for i in range(num_rooms):
+        poly = to_convex_contour(20)
+        room_size = 0.5 * min(la.norm(rooms[i] - rooms[(i+1)%num_rooms]), la.norm(rooms[i] - rooms[i-1]))
+        poly *= room_size
+        poly = np.array([point + rooms[i] for point in poly])
+        polygon_list.append(shapely.geometry.Polygon(poly))
+        min_room_size = min(min_room_size, room_size)
+
+    # Get corridors
+    for (start, end) in corridors:
+        corridor_direction = rooms[start] - rooms[end]
+        corridor_width_direction = np.array([corridor_direction[1], -corridor_direction[0]])
+        corridor_width_direction /= la.norm(corridor_width_direction)
+        corridor_width = min_room_size * 0.1
+        corridor_point_list = [rooms[start] + corridor_width_direction * corridor_width,
+                               rooms[end] + corridor_width_direction * corridor_width,
+                               rooms[end] - corridor_width_direction * corridor_width,
+                               rooms[start] - corridor_width_direction * corridor_width]
+        polygon_list.append(shapely.geometry.Polygon(corridor_point_list))
+
+    map = cascaded_union(polygon_list)
+
+    plt.plot(*map.exterior.xy)
+    plt.show()
+
+    return map.exterior.xy
 
 
-from shapely.ops import cascaded_union
+def makeCollection():
+    return
 
-polygon_list = []
-min_room_size = 800
-# Get convex rooms
-for i in range(num_rooms):
-    poly = to_convex_contour(20)
-    room_size = 0.5 * min(la.norm(corridors_map[i] - corridors_map[(i+1)%num_rooms]), la.norm(corridors_map[i] - corridors_map[i-1]))
-    poly *= room_size
-    poly = np.array([point + corridors_map[i] for point in poly])
-    polygon_list.append(shapely.geometry.Polygon(poly))
-    min_room_size = min(min_room_size, room_size)
-
-# Get corridors
-for i in range(num_rooms):
-    corridor_direction = corridors_map[i] - corridors_map[i-1]
-    corridor_width_direction = np.array([corridor_direction[1], -corridor_direction[0]])
-    corridor_width_direction /= la.norm(corridor_width_direction)
-    corridor_width = min_room_size * 0.1
-    corridor_point_list = [corridors_map[i] + corridor_width_direction * corridor_width, corridors_map[i-1] + corridor_width_direction * corridor_width, corridors_map[i-1] - corridor_width_direction * corridor_width, corridors_map[i] - corridor_width_direction * corridor_width]
-    polygon_list.append(shapely.geometry.Polygon(corridor_point_list))
-
-cascaded_union(polygon_list)
-
+if __name__ == '__main__':
+    num_rooms = 15
+    corridors, rooms = random_tree(num_rooms)
+    hobbit_hole = genTreeEnv(num_rooms, rooms, corridors)
+    polys = generatePolygonsGrid(5, 0.1, 0.4, 0.1, 0.5)
 
 # # A note on hallways
 # 
@@ -248,9 +282,4 @@ cascaded_union(polygon_list)
 # To go down a hallway with parallel edges, your robot needs either a strategy of two alternating fixed bounce rules, one repeating monotonic bounce rule, or two alternating relative bounce rules (and there are some tricky caveats on initial conditions with relative bounce rules). See WAFR 2020 paper for more details.
 # 
 # Hallways with non-parallel edges are different. See Thiffeault Microorganism Billiards for the original inspiration for this (figure 13). 
-
-# In[ ]:
-
-
-
 
